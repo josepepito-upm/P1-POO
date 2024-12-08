444b85d723a1c9c1b81f1632f235688abd8bab56
josepepito-upm <r.nuno@alumnos.upm.es>
2024-12-08 10:24:35 +0100

    comentario en Admin Commands
    Co-authored-by: Regem27 <Regem27@users.noreply.github.com>

--- gestordeportivo/src/main/java/practica1/gestordeportivo/commands/AdminCommands.java
+++ gestordeportivo/src/main/java/practica1/gestordeportivo/commands/AdminCommands.java
@@ -5,7 +5,7 @@ import practica1.gestordeportivo.types.Role;
 import practica1.gestordeportivo.views.CLIView;
 
 abstract class AdminCommands implements CommandInterface {
-
+    //¿Esto no debería de implementarse solamente en las views?
     CLIView cliView;
 
     public abstract Errors execute(String command);
