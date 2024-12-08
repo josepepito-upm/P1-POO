2f021f2dcf6e9cae4103cfa492ad8f0576ed0c69
josepepito-upm <r.nuno@alumnos.upm.es>
2024-12-08 10:25:34 +0100

    Cambios en TournamentMatchmaking y tal

--- gestordeportivo/src/main/java/practica1/gestordeportivo/commands/ManualTournamentMatchmaking.java
+++ gestordeportivo/src/main/java/practica1/gestordeportivo/commands/ManualTournamentMatchmaking.java
@@ -1,16 +1,14 @@
 package practica1.gestordeportivo.commands;
 import practica1.gestordeportivo.controllers.MatchmakeController;
-import practica1.gestordeportivo.controllers.TournamentController;
-import practica1.gestordeportivo.models.CommandLineInterpreter;
 import practica1.gestordeportivo.types.Errors;
 import practica1.gestordeportivo.types.MatchmakeModes;
 
 public class ManualTournamentMatchmaking extends TournamentMatchmaking {
     
-    
     public Errors validate(String command) {
         super.validate(command);        
-        String[] parts = command.split(" ");
+        
+        String[] parts = getParts(command);
         
         for (String string : parts) {
                 //También se puede poner como parts[2] si queremos que el -m se ponga en un formato específico
@@ -22,7 +20,10 @@ public class ManualTournamentMatchmaking extends TournamentMatchmaking {
     }
 
     public Errors execute(String command) {
-        MatchmakeController matchmakeController = new MatchmakeController(cli);
-        return validate(command).isNull() ? matchmakeController.manualMatchmake(command, command);
-    }
+        MatchmakeController matchmakeController = new MatchmakeController(super.getCli());
+        if(validate(command).isNull()) {
+            matchmakeController.manualMatchmake(getPlayerData(getParts(command))[0], getPlayerData(getParts(command))[1]);
+            return Errors.NULL;
+        } else return validate(command);
+    }   
 }
--- gestordeportivo/src/main/java/practica1/gestordeportivo/commands/TournamentMatchmaking.java
+++ gestordeportivo/src/main/java/practica1/gestordeportivo/commands/TournamentMatchmaking.java
@@ -1,20 +1,27 @@
 package practica1.gestordeportivo.commands;
 
-import practica1.gestordeportivo.controllers.MatchmakeController;
 import practica1.gestordeportivo.models.CommandLineInterpreter;
 import practica1.gestordeportivo.models.Player;
 import practica1.gestordeportivo.types.Errors;
 
-public class TournamentMatchmaking extends AdminCommands {
+public abstract class TournamentMatchmaking extends AdminCommands {
     protected CommandLineInterpreter cli;    
-    public Errors validate(String command) {
+    
+    protected CommandLineInterpreter getCli() {
+        return this.cli;
+    }
+    protected String[] getParts (String command) {
+        return command.split(" ");
+    }        
+    protected String[] getPlayerData (String[] parts) {
+            return parts[3].split(";");
+    }
+    public Errors validate(String command) {        
         super.validate(command);
-
         //ERRORES: FORMATO INCORRECTO POR ESPACIOS, FORMATO INCORRECTO POR ARGUMENTOS, UNO DE LOS JUGADORES YA ESTA EMPAREJADO, EL TORNEO NO ESTA EN CURSO
         //los errores ya estan en el enumerado
-        String[] parts = command.split(" ");
-        String[] playerData = parts[1].split(";");
-
+        String[] parts = this.getParts(command);
+        String[] playerData = this.getPlayerData(parts);        
         if (playerData.length < 4 || parts.length != 2) {
             return Errors.FORMAT_ERROR;
         }
@@ -24,16 +31,12 @@ public class TournamentMatchmaking extends AdminCommands {
                 return Errors.EXISTING_PLAYER;
             }
         }
-        return null;
+        return Errors.NULL;
     }
 
-    public Errors execute(String command) {
-        MatchmakeController matchmakeController = new MatchmakeController(cli.getPlayerList(), cli.getMatchmakeList());
-        if(validate(command) == null) {
+
+    public abstract Errors execute(String command); 
             
-            return null;
-        } else return validate(command);
-    }
 
     
 }
