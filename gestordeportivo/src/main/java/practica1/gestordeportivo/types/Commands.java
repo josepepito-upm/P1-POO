99b06959acb6830b28fa9dd6847118ab8103ff94
Regem27 <mario.reyes@alumnos.upm.es>
2024-12-20 00:14:29 +0100

    propuesta para Commands

--- gestordeportivo/src/main/java/practica1/gestordeportivo/types/Commands.java
+++ gestordeportivo/src/main/java/practica1/gestordeportivo/types/Commands.java
@@ -1,36 +1,41 @@
 package practica1.gestordeportivo.types;
 
+import java.util.function.Function;
+
 import practica1.gestordeportivo.commands.*;
 import practica1.gestordeportivo.models.CommandLineInterpreter;
 
 public enum Commands {
-    CREATE_PLAYER("player-create", new PlayerCreate()), 
-    CREATE_TEAM("team-create", new TeamCreate()),
-    CREATE_TOURNAMENT("tournament-create", new TournamentCreate()),
-    DELETE_PLAYER("player-delete", new PlayerDelete()),
-    DELETE_TEAM("team-delete", new TeamDelete()),
-    DELETE_TOURNAMENT("tournament-delete", new TournamentDelete()),
-    ADD_TEAM("team-add", new TeamAdd()),
-    REMOVE_TEAM("team-remove", new TeamRemove()),
+    CREATE_PLAYER("player-create", PlayerCreate::new),
+    CREATE_TEAM("team-create", TeamCreate::new),
+    CREATE_TOURNAMENT("tournament-create", TournamentCreate::new),
+    DELETE_PLAYER("player-delete", PlayerDelete::new),
+    DELETE_TEAM("team-delete", TeamDelete::new),
+    DELETE_TOURNAMENT("tournament-delete", TournamentDelete::new),
+    ADD_TEAM("team-add", TeamAdd::new),
+    REMOVE_TEAM("team-remove", TeamRemove::new),
     MATCHMAKING("tournament-matchmaking", MatchmakeModes.commandInterface),
-    ADD_TOURNAMENT("tournament-add", new TournamentAdd()),
-    REMOVE_TOURNAMENT("tournament-remove", new TournamentRemove()),
-    SHOW_STATISTICS("statistics-show", new StatisticsShow()),
-    LIST("tournament-list", new ListTournaments()),
+    DD_TOURNAMENT("tournament-add", TournamentAdd::new),
+    REMOVE_TOURNAMENT("tournament-remove", TournamentRemove::new),
+    SHOW_STATISTICS("statistics-show", StatisticsShow::new),
+    LIST("tournament-list", ListTournaments::new),
     LOGIN("login", new Login()),
     LOGOUT("logout", new Logout()),
-    SAVEALL("save-all", new SaveAll()),
-    RECOVERALL("recover-all", new RecoverAll());
-
+    SAVEALL("save-all", SaveAll::new),
+    RECOVERALL("recover-all", RecoverAll::new);
     private final String name;
     private final CommandInterface commandInterface;    
-    private final CommandLineInterpreter cli;
-    
-    Commands(String name, CommandInterface commandInterface) {
-        this.name = name;
-        this.commandInterface = commandInterface;
-    }
+    private final Function<CommandLineInterpreter, CommandInterface> commandFactory;
 
+    
+    Commands(String name, Function<CommandLineInterpreter, CommandInterface> commandFactory) {
+        this.name = name;
+        this.commandFactory = commandFactory;
+    }
+    
+    public void initialize(CommandLineInterpreter cli) {
+        this.commandInterface = commandFactory.apply(cli);
+    }
     
     public CommandInterface getCommand() {
         return commandInterface;
